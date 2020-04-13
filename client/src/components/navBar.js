a4b417c65e0334808d1d0c298434fec51d22f899
clueking1 <wcoan300@gmail.com>
2020-04-13 15:40:01 -0400

    added logo in nav bar

--- client/src/components/navBar.js
+++ client/src/components/navBar.js
@@ -2,10 +2,13 @@ import React from 'react'
 
 function Navbar () {
     return (
-        <nav className="navbar navbar-expand-sm navbar-dark">
-            <a className="navbar-brand" href="/">Take Note</a>
+        <nav className="navbar navbar-expand-sm">
+            <a className="navbar-brand" href="/">
+                TN
+            </a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample03" aria-controls="navbarsExample03" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
+            
             </button>
         </nav>
     )
